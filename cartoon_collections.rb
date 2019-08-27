2642fb5b5f0aa9f936715fbeae07d861a86ec640
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-27 23:30:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.map {|call| call.length > 4 ? (return true) : (return false) }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string_array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
